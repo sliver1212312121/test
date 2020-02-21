36ea0418193caae0295178573ded3a5e0a35740b
poohtrinityroots <60650887+poohtrinityroots@users.noreply.github.com>
2020-02-21 12:40:34 +0700

    testmariaconnection

---
+++ TestMariadbMysqlConnector.PY
@@ -0,0 +1,25 @@
+
+
+from datetime import datetime, timedelta
+import petl as etl
+import psycopg2
+import os
+import pandas as pd
+import mysql.connector
+
+connection1 = mysql.connector.connect(
+    host="203.154.28.234",
+    user="sarayuth",
+    passwd="3qZGA1QxqNp4PVpR",
+    db="doae_production_2018",
+    port='3306'
+)
+connection1.cursor()
+
+SQL_Query = pd.read_sql_query(
+    '''SELECT * FROM form_plant_1 limit 10''', connection1)
+
+df = pd.DataFrame(SQL_Query)
+
+master = df
+master
