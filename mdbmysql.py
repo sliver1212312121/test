4838c94c74200973e16901b574a63fa688836c54
poohtrinityroots <60650887+poohtrinityroots@users.noreply.github.com>
2020-02-21 17:06:54 +0700

    testmariaconnection

--- mdbmysql.py
+++ mdbmysql.py
@@ -8,7 +8,7 @@ import pandas as pd
 import mysql.connector
 print("hello")
 connection1 = mysql.connector.connect(
-    host="",
+    host="203.154.28.231",
     user="sarayuth",
     passwd="3qZGA1QxqNp4PVpR",
     db="doae_production_2018",
