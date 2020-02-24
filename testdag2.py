90206af2cf838b29f257dbaff2333fe38eba1c34
poohtrinityroots <60650887+poohtrinityroots@users.noreply.github.com>
2020-02-24 13:48:37 +0700

    testOracleconnection

--- testdag2.py
+++ testdag2.py
@@ -7,24 +7,24 @@ import os
 import pandas as pd
 import mysql.connector
 
-import cx_Oracle
+# import cx_Oracle
 
 # connection = cx_Oracle.connect('username/password@IP:Port/DatabaseName')
-connection = cx_Oracle.connect('SPATIAL/LaItApS@10.3.33.6:3333/Profile_DOAE')
-print("connection success")
+# connection = cx_Oracle.connect('SPATIAL/LaItApS@10.3.33.6:3333/Profile_DOAE')
+# print("connection success")
 
-# connection1 = mysql.connector.connect(
-#     host = "203.154.28.234",
-#     user = "sarayuth",
-#     passwd = "3qZGA1QxqNp4PVpR",
-#     db = "doae_production_2018"
-#     )
-# connection1.cursor()
-#
-# SQL_Query = pd.read_sql_query(
-#  '''SELECT * form_plant_1 limit 10''', connection1)
-#
-# df = pd.DataFrame(SQL_Query)
-#
-# master = df
-# master
+connection1 = mysql.connector.connect(
+    host = "203.154.28.234",
+    user = "sarayuth",
+    passwd = "3qZGA1QxqNp4PVpR",
+    db = "doae_production_2018"
+    )
+connection1.cursor()
+
+SQL_Query = pd.read_sql_query(
+ '''SELECT * form_plant_1 limit 10''', connection1)
+
+df = pd.DataFrame(SQL_Query)
+
+master = df
+master
