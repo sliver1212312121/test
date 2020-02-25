a2ae9eb521727e65f964b5e0bf1874057c57ca77
poohtrinityroots <60650887+poohtrinityroots@users.noreply.github.com>
2020-02-25 10:24:11 +0700

    upload oracle instantclient

--- testoracle.py
+++ testoracle.py
@@ -10,9 +10,20 @@ import os
 import cx_Oracle
 
 # connection = cx_Oracle.connect('username/password@IP:Port/DatabaseName')
-connection = cx_Oracle.connect('SPATIAL/LaItApS@10.3.33.6:3333/Profile_DOAE')
-print("connection success")
+# connection = cx_Oracle.connect('SPATIAL/LaItApS@10.3.33.6:3333/Profile_DOAE')
+# print("connection success")
 
+
+connection =cx_Oracle.connect('SPATIAL', 'LaItApS', "10.3.33.6:3333/doaedb",encoding="UTF-8")
+connection.cursor()
+
+SQL_Query = pd.read_sql_query(
+ '''select * from V_KS01 fetch first 1 row only''', connection)
+
+df = pd.DataFrame(SQL_Query)
+
+master = df
+master
 # connection1 = mysql.connector.connect(
 #     host = "203.154.28.234",
 #     user = "sarayuth",
